0e41de00f19173070cbbec80c62b98f672a91464
Guillaume AGNIERAY <dev@agnieray.net>
2025-12-08 14:32:18 +0100

    Bump version

--- _define.php
+++ _define.php
@@ -25,10 +25,10 @@ $this->register(
     'Galette Stripe',                                   //Name
     'Stripe integration',                               //Short description
     'Mathieu PELLEGRIN, manuelh78, Guillaume AGNIERAY', //Author
-    '1.0.0-dev',                                        //Version
+    '1.0.0-alpha1',                                     //Version
     '1.2.1',                                            //Galette compatible version
     'stripe',                                           //routing name and translation domain
-    '2025-07-25',                                       //Release date
+    '2025-12-08',                                       //Release date
     [   //Permissions needed
         'stripe_preferences'        => 'staff',
         'store_stripe_preferences'  => 'staff',
